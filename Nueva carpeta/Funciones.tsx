8c986a3cc981e325fc43515c236baafbe6fdfdc2
Diego26194 <ianchinadiego@gmail.com>
2025-10-16 22:20:06 -0300

    Correcciones varias

---
+++ Nueva carpeta/Funciones.tsx
@@ -0,0 +1,342 @@
+import {initRos, pubTopic, subTopic, unsubTopic } from './RosService2';
+
+
+//////////////////////////    Section1     /////////////////////////
+
+//Envia Orden para traer puntos desde la libreria de punto (Refrescar)
+export function cargarPuntosDB() {
+  const msg = {
+    orden: ["subir_db"],
+    coordenadas: []
+  };
+
+  console.log(" Enviando a /orden_web:", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+//Agrega Punto a Libreria de Puntos (Puesto ahora en Section2)
+export function agregarPuntoDB(nombrePunto:string, coord:number[]) {
+  const msg = {
+    orden: ['agregar', nombrePunto],
+    coordenadas: coord
+  };
+
+  console.log(" Enviando a /orden_web: agregando punto", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+// Elimina Punto seleccionado
+export function elimiarPuntoDB(nombrePunto:string) {
+  if (nombrePunto){
+    const confirmar = confirm(`¿Estás seguro de querer eliminar el Punto "${nombrePunto}"?`);
+    if (confirmar){
+      const msg = {
+        orden: ['eliminar', nombrePunto],
+        coordenadas: []
+      };
+
+      console.log(" Enviando a /orden_web: ELIMIAR", msg);
+
+      // Publica en ROS
+      pubTopic('orden_web', msg);
+    } 
+  }    
+}
+
+// Mostrar punto
+export function mostrarPuntoDB(nombrePunto:string) {
+  if (nombrePunto){
+    const msg = {
+      orden: ['ver', nombrePunto],
+      coordenadas: []
+    };
+    console.log(" Enviando a /orden_web: ELIMIAR", msg);
+
+    // Publica en ROS
+    pubTopic('orden_web', msg);
+  } 
+}
+
+// carga rutina en "rutina_actual"
+export function cargarRutinaRA(nombreRutina:string) {
+  if (nombreRutina){
+    const msg = {
+      orden: ['cargarRRA', nombreRutina],
+      coordenadas: []
+    };
+    console.log(" Enviando a /orden_web:", msg);
+
+    // Publica en ROS
+    pubTopic('orden_web', msg);
+  } 
+}
+
+//Resibe lista con todos los puntos de la libreria
+export function nombrePuntoDB(callback: (msg: any) => void) {
+  subTopic("lista_puntosdb", callback);
+  
+}
+
+//Resibe lista con todos los puntos de la libreria
+export function nombreRutinaDB(callback: (msg: any) => void) {
+  subTopic("lista_rutinasdb", callback);
+
+}
+
+
+export function elimiarRutinaDB(nombreRutina:string) {
+  console.log("a",nombreRutina,"no");
+  if (nombreRutina){
+    const confirmar = confirm(`¿Estás seguro de querer eliminar esta Rutina "${nombreRutina}"?`);
+    if (confirmar){
+      const msg = {
+        orden: ['eliminarRutina', nombreRutina],
+        coordenadas: []
+      };
+      console.log(" Enviando a /orden_web: ELIMIAR", msg);
+
+      // Publica en ROS
+      pubTopic('orden_web', msg);
+    } 
+  }  
+}
+
+
+export function agregarRutinaTabla(nombreRutina:string) { //agregar posicion despues
+  console.log("a",nombreRutina,"no");
+  if (nombreRutina){
+    const msg = {
+      orden: ['addRT', nombreRutina],
+      coordenadas: [] //luego agregar posicion aca coordenadas: [posicion]
+    };
+    console.log(" Enviando a /orden_web: agregar", msg);
+
+    // Publica en ROS
+    pubTopic('orden_web', msg);
+  }  
+}
+
+//Envia Orden para traer puntos desde la libreria de punto (Refrescar)
+export function cargarRutinasDB() {
+  const msg = {
+    orden: ['subirR_db'],
+    coordenadas: []
+  };
+
+  console.log(" Enviando a /orden_web:", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+//////////////////////////    FIN Section1     /////////////////////////
+
+
+//////////////////////////    Section2     ////////////////////////////
+
+
+export function correrTAngular( coord:number[],descriptcion:number[]) {
+  const msg = {
+    descriocion:descriptcion,
+    coordenadas: coord
+  };
+
+  console.log(" Enviando a /cord_ros: correr angulos", msg);
+
+  // Publica en ROS
+  pubTopic('cord_ros', msg);
+}
+
+export function correrTCartesiano( coord:number[]) {
+  const msg = {
+    data: coord
+  };
+
+  console.log(" Enviando a /cord_ros: correr cartesianos", msg);
+
+  // Publica en ROS
+  pubTopic('cord_ros', msg);
+}
+
+export function escucharPuntoDB(callback: (msg: any) => void) {
+  subTopic("puntodb", callback);
+
+  // ❌ Luego analizar si conviene cerrarlo o cuando hacerlo
+}
+
+export function escucharCordReal(callback: (msg: any) => void) {
+  subTopic("pos_real", callback);
+
+  // ❌ Luego analizar si conviene cerrarlo o cuando hacerlo
+}
+
+//////////////////////////    FIN Section2     /////////////////////////
+
+export function agregarPuntoRutina(nombrePunto:string,plan:string, coord:number[]) {
+  const msg = {
+    orden: ['addPT', nombrePunto,plan],
+    coordenadas: coord
+  };
+
+  console.log(" Enviando a /orden_web: agregando punto rutina", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+//////////////////////////    Section3     ////////////////////////////
+
+
+export function resibirMsgRutina(callback: (msg: any) => void) {
+  console.log("verificando");
+  subTopic("puntoRutina", callback);
+}
+
+//Elimina Punto de Rutina
+export function elimiarPuntoRutina(nombrePunto:string[], posiciones:number[]) {
+  const msg = {
+    orden: ['eliminarPunRut', ...nombrePunto],
+    coordenadas: posiciones
+  };
+
+  console.log("del", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+//Refresca Rutina actual
+export function refrescarRutina() {
+  const msg = {
+    orden: ['refresRut'],
+    coordenadas:[]
+  };  
+
+  console.log("refrescar", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+export function editarPuntoRutina(nombrePunto:string,plan:string, coord:number[]) {
+  const msg = {
+    orden: ['editPR', nombrePunto,plan],
+    coordenadas: coord
+  };
+
+  console.log(" Enviando a /orden_web: editando punto rutina", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+export function ejecutarRutina() {
+  const msg = { data: true };
+
+  console.log(" Correr rutina", msg);
+
+  // Publica en ROS
+  pubTopic('rutina', msg);
+}
+
+//Agregar una rutina(la rutina actual la guarda asignandole nombre)
+export function agregarRutina(nombreRutina:string) {
+  const msg = {
+    orden: ['addRutina', nombreRutina],
+    coordenadas:[]
+  };  
+
+  console.log("addRutina", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+//////////////////////////    General     ////////////////////////////
+
+export function refreshCordAng(coord:number[]) {
+  const msg = {
+    orden: ['ang-cart'],
+    coordenadas: coord
+  };
+
+  console.log(" Enviando a /orden_web: editando punto rutina", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+export function refreshCordCart(coord:number[]) {
+  const msg = {
+    orden: ['cart-ang'],
+    coordenadas: coord
+  };
+
+  console.log(" Enviando a /orden_web: editando punto rutina", msg);
+
+  // Publica en ROS
+  pubTopic('orden_web', msg);
+}
+
+export function publicar_informe(informe: string) {
+  const msg = { data: informe };
+
+  console.log(" informar", msg);
+
+  // Publica en ROS
+  pubTopic('informe_web', msg);
+}
+
+//Resibe mensaje de informe
+export function msgInforme(callback: (msg: any) => void) {
+  subTopic("informe_web", callback);
+  
+}
+
+export function msgEmergente(tipoMsg: string) {
+  let mensaje: string = "";   // inicializado en vacío
+
+  switch (tipoMsg) {    
+    case 'errorR':
+      mensaje = [
+        "⚠ Rutina no encontrada en base de datos lo que infiere una desincronizacion con esta.",
+          "🔄 Refresque la tabla para asegurarse de trabajar con los datos reales.",
+      ].join("\n");
+      break;
+
+    case 'errorRR':
+      mensaje = [
+        "⚠ La rutina cargada contiene rutinas no existentes entre sus instrucciones.",
+      ].join("\n");
+      break;
+
+    case 'errorP':
+      mensaje = [
+        "⚠ Punto no encontrado en base de datos lo que infiere una desincronizacion con esta.",
+          "🔄 Refresque la tabla para asegurarse de trabajar con los datos reales.",
+      ].join("\n");
+      break;
+
+    case 'delPR':
+      // lógica si eliminás bien
+      break;
+
+    case 'ErrordelPR':
+      mensaje = [
+        "⚠ No se eliminó ninguna fila, algunas instrucciones no coinciden con la base de datos lo que infiere una desincronizacion con esta.",
+        "🔄 Refresque la tabla para asegurarse de trabajar con los datos reales.",
+      ].join("\n");
+      break;
+
+    default:
+      console.warn("Acción no reconocida:", tipoMsg);
+  }
+
+  if (mensaje) {
+    alert(mensaje);
+  }
+};
---
+++ Nueva carpeta/Section3.tsx
@@ -0,0 +1,239 @@
+import React, { useState, useRef } from "react";
+import {
+  Typography,
+  Box,
+  IconButton,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+  Grid,
+  Button,
+  Stack,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions
+} from "@mui/material";
+import RutineTable, { RutineTableRef } from '../../Pages/RutineTable';
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+
+import CompactsInputs from "../Elements/Inputs/CompactsInput";
+import Button1 from "../Elements/Buettons/Button1";
+import ButtonDelete from "../Elements/Buettons/ButtonDelete";
+import ButtonEdit from "../Elements/Buettons/ButtonEdit";
+import ButtonAdd from "../Elements/Buettons/ButtonAdd";
+import ButtonSave from "../Elements/Buettons/ButtonSave";
+import ButtonReflesh from "../Elements/Buettons/ButtonReflesh";
+import SelectRutina from "../Elements/SelectRutina";
+import InputCord from "../Elements/Inputs/InputCord";
+import InputPositive from "../Elements/Inputs/InputPoisitive";
+
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+
+import Section4 from "../Section1/Section4";
+import { ejecutarRutina, refrescarRutina, agregarRutina } from "../../Services/Funciones";
+
+
+interface Section3Props {
+  setFlagAddRutine: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const Section3: React.FC<Section3Props> = ({ setFlagAddRutine }) => {
+  const [open, setOpen] = useState(false);
+  const [flagEliminarPRut, setFlagEliminarPRut] = useState(false);
+  const [wait, setWait] = useState(0);
+  const tableRef = useRef<RutineTableRef>(null);
+  const [showLibrarie, setShowLibrarie] = useState(false);
+  const [nombreRutina, setNombreRutina] = useState("");
+  
+
+  const [flagAddPoint, setFlagAddPoint] = useState(false);
+
+  const handleAddRutineClick = () => {
+    //setFlagAddRutine(true);
+    agregarRutina(nombreRutina)
+  };
+
+  const handleDeleteRutineClick = () => {
+    setFlagEliminarPRut(true);
+  };
+
+  const handleAddClick = () => {
+    alert(`Click add`);
+  };
+
+  const handleEditClick = () => {
+    alert(`Click Edit`);
+  };
+
+  const handleDeleteClick = () => {
+    alert(`Click Delete`);
+  };
+
+  // Handler que recibe el valor actualizado del hijo
+  const handleWaitChange = (val: number) => {
+    setWait(val);
+  };
+  
+  // Handler que recibe el valor actualizado del hijo
+  const handleWaitPassDatagrid = () => {
+    if (tableRef.current) {
+      tableRef.current.updateWaitForSelectedRows(wait); 
+    }
+  };
+
+  const handleEjecutarClick = () => {
+    if (tableRef.current?.validarRutina()) {
+      ejecutarRutina();
+    } else {
+      alert("⚠ Hay filas en modo edición. Confirme o cancele antes de ejecutar.");
+    }
+  };
+
+
+  
+
+  <Dialog open={open} onClose={() => setOpen(false)}>
+    <DialogTitle>Subtítulo del diálogo</DialogTitle>
+    <DialogContent>
+      <Typography>Contenido del diálogo.</Typography>
+    </DialogContent>
+    <DialogActions>
+      <Button1 onClick={() => setOpen(false)}>Cerrar</Button1>
+    </DialogActions>
+  </Dialog>
+
+  return (
+    <Stack  spacing={0}
+      sx={{
+          width: '100%', 
+          height: '100%'
+        }}>
+          <Grid
+            container
+            spacing={1}
+            sx={{
+              width: '100%',
+              padding: 0,
+              height: '7%'
+            }}
+          >
+          <Grid size={{xs: 10, md: 10}}>
+            <Stack direction="row" spacing={1}
+              sx={{
+                width: '100%', //76%
+              }}>
+
+              <Typography variant="h6">Rutina</Typography>  
+
+              <CompactsInputs 
+                label="Nombre de Rutina" 
+                
+                fullWidth size="small"
+                value={nombreRutina}
+                onChange={(e) => setNombreRutina(e.target.value)}
+                sx={{width: '40%',
+                  height: '100%',
+                  padding: '4px 0px',   
+                  '& input': {
+                    padding: '0px 2px',
+                  },
+                }}
+              >              
+              </CompactsInputs>        
+
+              <ButtonSave description={'Guardar Rutina'} onClick={handleAddRutineClick}/>
+          
+              <Button1 title= "Ejecutar Rutina" sx={{width: '20%'}} variant="outlined" onClick={handleEjecutarClick}>
+                {<PlayArrowIcon />}Ejecutar
+              </Button1>    
+             {/*<ButtonAdd description={'Agregar Punto a la Rutina'} onClick={handleAddRutineClick}/> */}
+              <ButtonDelete description={'Eliminar Puntos de la Rutina'}onClick={handleDeleteRutineClick}/>                          
+                
+              <InputPositive
+                label="Wait" 
+                valMin={0}
+                valMax={100}
+                value={wait}
+                onChange={handleWaitChange}
+                sx={{
+                  fontSize: "0.9rem",
+                  padding: "0px 1px",
+                  minHeight: "10px",
+                  maxHeight: "30px",
+                  textAlign: "center",
+                  pt: 1,
+                }}
+              />     
+              <Button1 sx={{width: '20%'}} variant="outlined" onClick={handleWaitPassDatagrid}>
+                add
+              </Button1>
+
+              
+              <ButtonReflesh description={'Refrescar tabla de rutina'}onClick={refrescarRutina }/> 
+
+              
+            </Stack>
+
+            
+          </Grid>
+
+          <Grid size={{xs: 2, md: 2}}>
+            <Stack direction="row" spacing={0}>
+              
+              {/* 
+              <SelectRutina onAdd={handleAddClick} onEdit={handleEditClick} onDelete={handleDeleteClick }/>
+
+              <IconButton
+                  onClick={() => setShowLibrarie(prev => !prev)}
+                  size="small"
+                  sx={{ "&:focus": { outline: "none" }, padding: '0px 0px', }}
+                >
+                  {showLibrarie ?<ExpandLessIcon/> : <ExpandMoreIcon />}
+                  
+              </IconButton>
+            */}
+            </Stack>
+            
+          </Grid>
+        </Grid>  
+
+        <Grid
+          container
+          spacing={1}
+          sx={{
+            width: '100%',
+            padding: 0,
+            height: '93%'
+          }}
+        >
+          <Grid size={{xs: showLibrarie ? 10 : 12, md: showLibrarie ? 10 : 12}} >
+            <RutineTable 
+              flagEliminarPRut={flagEliminarPRut} 
+              setFlagEliminarPRut={setFlagEliminarPRut} 
+              ref={tableRef} 
+            />
+          </Grid>
+          {/* 
+          {showLibrarie && (
+          <Grid size={{xs: 2., md: 2.}} sx={{ height: '100%' , borderRight: '3px solid #aaa'}} >
+            <Stack spacing={2} sx={{ height: '100%', overflowY: 'auto',}}>
+              <Box sx={{ height: '100%', maxHeight: "100%"}}> 
+                <Section4 setFlagAddPoint={setFlagAddPoint} />
+              </Box>
+            </Stack>
+          </Grid>
+          )}
+          */}
+        </Grid>
+        
+        
+    </Stack>
+  );
+};
+
+export default Section3;
